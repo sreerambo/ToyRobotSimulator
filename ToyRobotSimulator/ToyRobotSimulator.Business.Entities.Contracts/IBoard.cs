8fdf2a519e3985a440ade5959b00a009679e1167
Unknown <sreeram.sreedharan@gmail.com>
2018-01-22 21:50:13 +1300

    Modelled robot and board
    Split responsibilities between different entities.

---
+++ ToyRobotSimulator/ToyRobotSimulator.Business.Entities.Contracts/IBoard.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToyRobotSimulator.Business.Entities.Contracts
+{
+    public interface IBoard
+    {
+        bool IsValid(IPosition position);
+    }
+}
---
+++ ToyRobotSimulator/ToyRobotSimulator.Business.Entities.Contracts/IPosition.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToyRobotSimulator.Business.Entities.Contracts
+{
+    public interface IPosition
+    {
+        int X { get; set; }
+        int Y { get; set; }
+    }
+}
