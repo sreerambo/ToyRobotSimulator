88e060b20aeaf9348e6dd2a43a4c717ab7f057a0
Unknown <sreeram.sreedharan@gmail.com>
2018-01-31 22:33:46 +1300

    Added mapper for Direction and vectors

--- ToyRobotSimulator.Business.Entities/Utils/DirectionVectorMapper.cs
+++ ToyRobotSimulator.Business.Entities/Utils/DirectionVectorMapper.cs
@@ -9,14 +9,50 @@ namespace ToyRobotSimulator.Business.Entities.Utils
 {
     public class DirectionVectorMapper : IBidirectionalMapper<Direction, Vector2>
     {
+        private static Vector2 NorthVector = new Vector2(0, 1);
+        private static Vector2 SouthVector = new Vector2(0, -1);
+        private static Vector2 EastVector = new Vector2(1, 0);
+        private static Vector2 WestVector = new Vector2(-1, 0);
+
         public Direction Map(Vector2 toMap)
         {
-            throw new NotImplementedException();
+            if (toMap.Equals(NorthVector))
+            {
+                return Direction.North;
+            }
+            else if (toMap.Equals(SouthVector))
+            {
+                return Direction.South;
+            }
+            else if (toMap.Equals(EastVector))
+            {
+                return Direction.East;
+            }
+            else if (toMap.Equals(WestVector))
+            {
+                return Direction.West;
+            }
+            else
+            {
+                throw new ArgumentException("Vector couldn't be mapped", nameof(toMap));
+            }    
         }
 
         public Vector2 Map(Direction toMap)
         {
-            throw new NotImplementedException();
+            switch(toMap)
+            {
+                case Direction.North:
+                    return NorthVector;
+                case Direction.South:
+                    return SouthVector;
+                case Direction.East:
+                    return EastVector;
+                case Direction.West:
+                    return WestVector;
+                default:
+                    throw new ArgumentException("Direction couldn't be mapped", nameof(toMap));
+            }
         }
     }
 }
